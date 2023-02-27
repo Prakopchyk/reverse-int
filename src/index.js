75a0e52bd29e7c308f41236d1fda2db0472be021
Prakopchyk <prokopchike1987@gmail.com>
2023-02-27 19:33:55 +0100

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let str = n.toString().split('').reverse().join('');
+  if (n < 0) {
+    let newStr;
+    newStr = str.slice(0, [str.length - 1]);
+    return (`${newStr}`);
+  } else {
+    return str;
+  }
 }
